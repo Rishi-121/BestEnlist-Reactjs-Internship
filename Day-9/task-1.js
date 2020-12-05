c7cd4d5d4fa3c90904521ab6c60bb23a2a98ff3c
Rishi-121 <dashrushikesh1121@gmail.com>
2020-12-05 13:01:04 +0530

    Added the Day-9 Assignment file

---
+++ Day-9/task-1.js
@@ -0,0 +1,17 @@
+// Task 1
+
+// jshint esversion:6
+
+Array.prototype.begetduplicate = function () {
+  const duplicateValues = [];
+  this.forEach((value) => {
+    if (this.filter((x) => x === value).length > 1) {
+      duplicateValues.push(value);
+    }
+  });
+
+  return Array.from(new Set(duplicateValues));
+};
+
+var sampleinput = [1, 2, 4, 5, 2, 5];
+console.log(sampleinput.begetduplicate());
